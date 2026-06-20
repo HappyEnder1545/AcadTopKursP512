d72a572a7d2f68ef2696941adb5db2e326a017e5
HappyEnder1545 <anten0901@gmail.com>
2026-06-20 19:24:04 +0700

    Add table creation

--- KursBig/Kursach/Program.cs
+++ KursBig/Kursach/Program.cs
@@ -10,6 +10,60 @@ namespace Proj
 
         }
 
+        static void CreateGenreTbl()
+        {
+            string ConnString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Kurs;Integrated Security = True";
+            using var conn = new SqlConnection(ConnString);
+            conn.Open();
+
+            string sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Genre') 
+            CREATE TABLE Genre (
+            Id INT PRIMARY KEY IDENTITY(1,1),
+            Name VARCHAR(100) NOT NULL )";
+
+            using var comm = new SqlCommand(sql, conn);
+            comm.ExecuteNonQuery();
+            Console.WriteLine("Создана таблица жанров");
+        }
+        static void CreateAuthorTbl()
+        {
+            string ConnString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Kurs;Integrated Security = True";
+            using var conn = new SqlConnection(ConnString);
+            conn.Open();
+
+            string sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Author') 
+            CREATE TABLE Author (
+            Id INT PRIMARY KEY IDENTITY(1,1),
+            FirstName VARCHAR(100) NOT NULL,
+            LastName VARCHAR(100) NOT NULL )";
+
+            using var comm = new SqlCommand(sql, conn);
+            comm.ExecuteNonQuery();
+            Console.WriteLine("Создана таблица авторов");
+        }
+        static void CreateBookTbl()
+        {
+            string ConnString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Kurs;Integrated Security = True";
+            using var conn = new SqlConnection(ConnString);
+            conn.Open();
+
+            string sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Book') 
+            CREATE TABLE Book (
+            Id INT PRIMARY KEY IDENTITY(1,1),
+            Title VARCHAR(100),
+            AuthorId INTEGER NOT NULL REFERENCES Author(Id),
+            GenreId INTEGER NOT NULL REFERENCES Genre(Id),
+            Year INTEGER,
+            Pages INTEGER,
+            Status VARCHAR(50) NOT NULL DEFAULT 'Not Read',
+            Notes VARCHAR(100)
+            )";
+
+
+            using var comm = new SqlCommand(sql, conn);
+            comm.ExecuteNonQuery();
+            Console.WriteLine("Создана таблица книг");
+        }
         static async Task CreateBookAsync()
         {
 
