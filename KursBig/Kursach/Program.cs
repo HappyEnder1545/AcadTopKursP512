e38ca59aa25a2c4d4f61327f0701d895cffba13e
HappyEnder1545 <anten0901@gmail.com>
2026-06-20 17:34:37 +0700

    Small changes
    nothing special, just one func added

--- KursBig/Kursach/Program.cs
+++ KursBig/Kursach/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 
 namespace Proj
 {
@@ -9,7 +10,38 @@ namespace Proj
 
         }
 
+        static async Task CreateBookAsync()
+        {
 
+        }
+        static async Task ReadBookAsync()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Kurs;Integrated Security = True";
+            string sql = "SELECT Id, Title, AuthorId, GenreId, Year, Pages, Status, Notes";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            int id = reader.GetInt32(0);
+                            string title = reader.GetString(1);
+                            string lastName = reader.GetString(2);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+        static async Task UpdateBookAsync()
+        {
+
+        }
     }
 
 
